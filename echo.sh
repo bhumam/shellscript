91a29ad654865688681b1ac3b4e24a55bd0c25e2
purandhar <purandharbhumam@gmail.com>
2024-04-23 12:27:40 +0530

    echo

--- echo.sh
+++ echo.sh
@@ -1,2 +1,3 @@
-#!/bin/bash
+#!/bin/bash #shebang is the location of shell interpretor. shell/linux commands written inside shellscript is interpreted and executed by this shibang.
+
 echo "hello! I'am learning Devops with AWS"
---
+++ variables2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#declaring variable
+person1=rahul
+person2=ravi #no spacing
+
+#referring variable
+echo "$PERSON1: Hello $PERSON2, How are you?"
+echo "${PERSON2}: Hi $PERSON1, I am fine. How are you?"
+echo "$PERSON1: I am fine too. how is your work?"
+echo "$PERSON2: not bad. I am thinking to upgrade to DevOps"
