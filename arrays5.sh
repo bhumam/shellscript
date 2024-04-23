af96800db3382ea728b971962a1729bc5d6f90be
purandhar <purandharbhumam@gmail.com>
2024-04-23 12:48:06 +0530

    echo

--- arrays5.sh
+++ arrays5.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 branch=("eee" "mech" "cse" "civil" "ece")
-echo "${branch[0]}
-echo "${branch[2]}
-echo "${branch[1]}
-echo "${branch[3]}
-echo "${branch[@]}
+echo "${branch[0]}"
+echo "${branch[2]}"
+echo "${branch[1]}"
+echo "${branch[3]}"
+echo "${branch[@]}"
