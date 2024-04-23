0fb7b0105d06eb0580cc37ebbb11a96e653cb93c
purandhar <purandharbhumam@gmail.com>
2024-04-23 12:40:04 +0530

    echo

---
+++ variables4.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "please enter username:"
+read -s USERNAME # user entry will be stored in USERNAME variable
+echo "please enter password:"
+read -s PASSWORD # user entry will be stored in PASSWORD variable
+echo "username:$USERNAME and password:$PASSWORD"
