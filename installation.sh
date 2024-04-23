50281c8b2e6c32f3ecf584bc0aad7d9f972af0c2
purandhar <purandharbhumam@gmail.com>
2024-04-23 13:38:44 +0530

    echo

---
+++ installation.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+echo "please get root access"
+else
+echo "You are a super user"
+fi
+dnf install mysql -y
+if [ $? -ne 0]
+then
+echo "install...Fail"
+exit 1
+else
+echo "install...success"
+fi
+dnf install mgit -y
+if [ $? -ne 0]
+then
+echo "install...Fail"
+exit 1
+else
+echo "install...success"
+fi
+echo "is script proceeding?"
